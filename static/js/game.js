fda3ce70f1d47b959163acec88d4d32bf0b36a24
adamRajmuller <rajfta@gmail.com>
2019-03-29 19:20:06 +0100

    working

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,27 @@
-alert('You should write code into "static/js/game.js" to make it work');
+const gameCells = document.getElementsByClassName('game-cell');
+const table = document.getElementById('game-board');
+
+
+let playerTurn = 0;
+function getMark () {
+    if (playerTurn) {
+        playerTurn = !playerTurn;
+        return `O`
+    }
+    else {
+        playerTurn = !playerTurn;
+        return `X`
+    }
+    }
+
+for (gameCell of gameCells) {
+    gameCell.addEventListener('click', function (e) {
+        if (e.target.innerHTML === '') {
+            e.target.innerHTML = getMark()
+    }});
+}
+
+function winChecker() {
+    let counter = 0;
+    for
+}
